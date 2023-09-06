de853f4681df4289de6b8ac7f91452fe5eba1a2c
Ian Mesquita <ianmesquitadb@gmail.com>
2023-09-05 22:52:10 -0300

    feat: Adicionando configurações para gravação de logs da aplicação.

--- src/main/java/com/redhat/leeds/application/service/KafkaProducerService.java
+++ src/main/java/com/redhat/leeds/application/service/KafkaProducerService.java
@@ -1,15 +1,22 @@
 package com.redhat.leeds.application.service;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class KafkaProducerService {
+    private static final Logger logger = LoggerFactory.getLogger(KafkaProducerService.class);
+
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String topic, String message) {
+        log.info("Enviando mensagem para a fila: " + message);
         kafkaTemplate.send(topic, message);
     }
 }
